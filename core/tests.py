8412f8708f026c270c215e62f3d24d93e908fc6e
RodrigoPaulaLopes <rodrigo.plopesti@gmail.com>
2024-02-14 03:27:44 -0300

    test: create tests

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.test import Client, TestCase
 
-# Create your tests here.
+
+class IndexTest(TestCase):
+    
+    def setUp(self) -> None:
+        client = Client()
+        
+    def tearDown(self) -> None:
+        pass
+        
+    def index_status_200(self):
+        
+        resp = self.client.get('/')
+        
+        self.assertEquals(resp.status_code, 200)
+        
+    def index_template_used(self):
+     
+        resp = self.client.get('/')
+        
+        self.assertTemplateUsed(resp, 'index.html')
+
+
+
