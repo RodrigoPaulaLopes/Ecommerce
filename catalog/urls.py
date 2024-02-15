d545acb774d435ae4bd6bcc54ebca1458f73993d
RodrigoPaulaLopes <rodrigo.plopesti@gmail.com>
2024-02-14 23:43:19 -0300

    feat: list products by category

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import product_list
+from .views import product_list, category
 
 urlpatterns = [
-    path("", product_list, name='produtos')
+    path("", product_list, name='produtos'),
+    path("<str:slug>", category, name='produtos')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -2,10 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from .models import Product
+from .models import Product, Category
 def product_list(request):
     
     context = {
         'products': Product.objects.all()
     }
     return render(request, 'catalog/product_list.html', context)
+
+def category(request, slug):
+    cat = Category.objects.get(slug=slug)
+    context = {
+        'products': Product.objects.filter(category=cat),
+        'current_category': cat
+    }
+    return render(request, 'catalog/category.html', context)
