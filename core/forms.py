0359d3a863d7f96ed8eb7eef67d23ae3c802d3af
RodrigoPaulaLopes <rodrigo.plopesti@gmail.com>
2024-02-18 13:49:42 -0300

    feat: create send email function

--- core/forms.py
+++ core/forms.py
@@ -1,7 +1,7 @@
 
 from django import forms
-
-
+from django.core.mail import send_mail
+from django.conf import settings
 
 class ContactForms(forms.Form):
     nome = forms.CharField(label='nome', )
@@ -14,3 +14,11 @@ class ContactForms(forms.Form):
     #     self.fields['nome'].widget.attrs['class'] = 'form-control'
     #     self.fields['email'].widget.attrs['class'] = 'form-control'
     #     self.fields['message'].widget.attrs['class'] = 'form-control'
+    
+    def send_mail(self):
+        nome = self.cleaned_data['nome']
+        email = self.cleaned_data['email']
+        message = self.cleaned_data['message']
+        
+        # i need to configure this
+        # send_mail(subject=nome, message=message, from_email=settings.DEFAULT_FROM_EMAIL, recipient_list=[settings.DEFAULT_FROM_EMAIL])
--- core/views.py
+++ core/views.py
@@ -1,17 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 from .forms import ContactForms
 def index(request):
   return render(request, 'index.html')
 
 def contact(request):
-  
-  if request.method == 'POST':
-    form = ContactForms(request.POST)
-  else:
-    form = ContactForms()
+  form = ContactForms(request.POST or None)
+  success = False
+  if form.is_valid():
+    success = True
+  form = ContactForms()
   context = {
-    'form': form
+    'form': form,
+    'success': success
   }
   return render(request, 'contact.html', context)
